f99e1e0b8f83bedac9d4e0847d51afb7ca9473de
FernandoJMartins <julio8martins@gmail.com>
2024-04-16 14:45:38 -0300

    add comment to posts

--- src/main/java/com/FernandoMartins/workshopmongo/config/Instantiation.java
+++ src/main/java/com/FernandoMartins/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.FernandoMartins.workshopmongo.domain.Post;
 import com.FernandoMartins.workshopmongo.domain.User;
 import com.FernandoMartins.workshopmongo.dto.AuthorDTO;
+import com.FernandoMartins.workshopmongo.dto.CommentDTO;
 import com.FernandoMartins.workshopmongo.repository.PostRepository;
 import com.FernandoMartins.workshopmongo.repository.UserRepository;
 
@@ -39,11 +40,18 @@ public class Instantiation implements CommandLineRunner{
 		Post p1 = new Post(null, sdf.parse("10/10/2023"),"Partiu voyage", "estou indo viajar", new AuthorDTO(maria));
 		Post p2 = new Post(null, sdf.parse("12/12/2022"),"Olá, boa tarde!", "Como vcs estão?", new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Parabéns, amigo",sdf.parse("10/10/2023"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite",sdf.parse("10/10/2023"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia",sdf.parse("10/10/2023"), new AuthorDTO(alex));
+		
+		p1.getComments().addAll(Arrays.asList(c1,c2));
+		p2.getComments().addAll(Arrays.asList(c3));
+		
 		post.saveAll(Arrays.asList(p1,p2));
 		
 		maria.getPosts().addAll(Arrays.asList(p1,p2));
 		repo.save(maria);
-		
+		 
 	}
 
 }
