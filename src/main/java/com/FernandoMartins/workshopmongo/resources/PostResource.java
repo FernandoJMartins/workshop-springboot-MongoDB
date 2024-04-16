3c8ea5ffef2770b1b00dbaf92e74621823b1d5c7
FernandoJMartins <julio8martins@gmail.com>
2024-04-16 15:07:52 -0300

    Query method and FindByTitle

--- src/main/java/com/FernandoMartins/workshopmongo/resources/PostResource.java
+++ src/main/java/com/FernandoMartins/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.FernandoMartins.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.FernandoMartins.workshopmongo.domain.Post;
+import com.FernandoMartins.workshopmongo.resources.util.URL;
 import com.FernandoMartins.workshopmongo.services.PostService;
 
 @RestController
@@ -21,6 +25,13 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = srv.findById(id);
 		return ResponseEntity.ok().body(post);
+	} 
+	
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = srv.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	} 
 
 }
