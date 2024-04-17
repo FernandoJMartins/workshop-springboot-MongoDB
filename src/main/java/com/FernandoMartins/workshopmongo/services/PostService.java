7d791c589ef4be095b9608df7a117dccd5373270
FernandoJMartins <julio8martins@gmail.com>
2024-04-16 23:00:46 -0300

    fullSearch

--- src/main/java/com/FernandoMartins/workshopmongo/services/PostService.java
+++ src/main/java/com/FernandoMartins/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.FernandoMartins.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,4 +26,8 @@ public class PostService {
 		return repo.findByTitle(text);
 	}
 
+	public List<Post> fullSearch(String text, Date min, Date max){
+		max = new Date(max.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, min, max);
+	}
 }
